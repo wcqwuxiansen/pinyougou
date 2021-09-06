06c2ee8fd5adf74f59b3c40b84ec41d06f12f2d8
tom_glb <tom_glb@atguigu.com>
2021-09-06 16:23:27 +0800

    9/6 update hotfix 23

--- pinyougou-web/src/main/java/com/heima/Demo1Servlet.java
+++ pinyougou-web/src/main/java/com/heima/Demo1Servlet.java
@@ -22,6 +22,7 @@ public class Demo1Servlet extends  HttpServlet {
         System.out.println("hello 品优购9/6更新1 hotfix");
         System.out.println("hello 品优购9/6更新12 hotfix");
         System.out.println("hello 品优购9/6更新123 hotfix");
+        System.out.println("hello 品优购9/6更新1234 hotfix");
     }
 
     @Override
