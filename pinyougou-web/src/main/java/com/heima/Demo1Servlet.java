d4376c8dd8e759595ae74c279c55cb90c9a0713e
wcqwuxiansen <89242284+wcqwuxiansen@users.noreply.github.com>
2021-09-24 16:52:11 +0800

    Update Demo1Servlet.java

--- pinyougou-web/src/main/java/com/heima/Demo1Servlet.java
+++ pinyougou-web/src/main/java/com/heima/Demo1Servlet.java
@@ -21,6 +21,7 @@ public class Demo1Servlet extends  HttpServlet {
         System.out.println("hello 品优购9/6更新1");
         System.out.println("哈哈哈哈");
         System.out.println("hello 品优购9/14更新1");
+        System.out.println("hello 品优购9/24更新 online");
     }
 
     @Override
